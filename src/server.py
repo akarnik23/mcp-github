7e8c11bfa24d055fd9a41e45d38ea9792c0f97c2
akarnik23 <akarnik23@gmail.com>
2025-10-13 20:09:59 -0700

    minor change for render deployment

--- src/server.py
+++ src/server.py
@@ -882,4 +882,5 @@ if __name__ == "__main__":
                 status_code=500
             )
     
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
